03f8f4deb325ac551c925e8eeeef16534fbe6738
ihwang <sheep9121@gmail.com>
2020-04-08 17:44:23 +0300

    Add one of line edition features     Ctrl_left : moving cursor to the first character of the previous word

--- src/ft_get_line.c
+++ src/ft_get_line.c
@@ -6,7 +6,7 @@
 /*   By: ihwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/29 19:13:18 by ihwang            #+#    #+#             */
-/*   Updated: 2020/04/08 16:39:42 by ihwang           ###   ########.fr       */
+/*   Updated: 2020/04/08 17:43:43 by ihwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -234,10 +234,12 @@ void				ctrl_down(t_l *l)
 	}
 }
 
-void				ctrl_right(t_l *l, int y_inc)
+void				ctrl_right(t_l *l)
 {
 	int				i;
+	int				y_inc;
 
+	y_inc = 0;
 	i = l->x + (l->y * l->co) - PMPT;
 	(ft_iswhite(l->line[i - 1]) && !ft_iswhite(l->line[i]) && i) ? i++ : 0;
 	while (i <= l->c_nb)
@@ -245,16 +247,14 @@ void				ctrl_right(t_l *l, int y_inc)
 		if ((ft_iswhite(l->line[i - 1]) && !ft_iswhite(l->line[i]) && i)
 				|| i == l->c_nb)
 		{
-			if (i + PMPT - (l->x + (l->y * l->co)) < l->co - l->x)
-				l->x = (i + PMPT) % l->co;
-			else
+			if (i + PMPT - (l->x + (l->y * l->co)) >= l->co - l->x)
 			{
 				l->y += y_inc;
-				l->x = (i + PMPT) % l->co;
 				while (y_inc--)
 					apply_termcap_str("do", 0, 0);
 			}
 			apply_termcap_str("ch", 0, (i + PMPT) % l->co);
+			l->x = (i + PMPT) % l->co;
 			break ;
 		}
 		((i + PMPT) % l->co == l->co - 1) ? y_inc++ : 0;
@@ -262,11 +262,42 @@ void				ctrl_right(t_l *l, int y_inc)
 	}
 }
 
-void				ctrl_left(t_l *l)
+void				ctrl_left_for_zero(t_l *l, int y_dec)
 {
+	l->x = PMPT;
+	l->y = 0;
+	while (y_dec--)
+		apply_termcap_str("up", 0, 0);
+	apply_termcap_str("ch", 0, PMPT);
+}
 
+void				ctrl_left(t_l *l, int y_dec)
+{
+	int				i;
+	int				curr;
 
-
+	i = l->x + (l->y * l->co) - PMPT;
+	curr = i;
+	ft_iswhite(l->line[i - 1]) && !ft_iswhite(l->line[i]) && i ? i-- : 0;
+	while (i >= 0)
+	{
+		if (i == 0)
+			ctrl_left_for_zero(l, y_dec);
+		else if (ft_iswhite(l->line[i - 1]) && !ft_iswhite(l->line[i]))
+		{
+			if (l->x < curr - i)
+			{
+				l->y -= y_dec;
+				while (y_dec--)
+					apply_termcap_str("up", 0, 0);
+			}
+				apply_termcap_str("ch", 0, (i + PMPT) % l->co);
+				l->x = (i + PMPT) % l->co;
+			break ;
+		}
+		(i + PMPT) % l->co == 0 ? y_dec++ : 0;
+		i--;
+	}
 }
 
 void				parse_key_esc(char t[], t_l *l)
@@ -289,9 +320,9 @@ void				parse_key_esc(char t[], t_l *l)
 	else if (!ft_strcmp(t, "\x1b[1;5B"))
 		ctrl_down(l);
 	else if (!ft_strcmp(t, "\x1b[1;5C"))
-		ctrl_right(l, 0);
-//	else if (!ft_strcmp(&tmp[1], "[1;5D"))
-//		ctrl_left(l);
+		ctrl_right(l);
+	else if (!ft_strcmp(t, "\x1b[1;5D"))
+		ctrl_left(l, 0);
 	else if (ft_isprint(t[0])/* && t[0] != '['*/)
 		add_key(t, l);
 }
