4a06fec5b0987cba272af745f3663fbc735c0357
xopxop <xopxop.1403@gmail.com>
2020-04-13 18:05:50 +0300

    modifed

---
+++ includes/minishell.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/26 11:22:29 by dthan             #+#    #+#             */
+/*   Updated: 2020/03/26 11:25:46 by dthan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "../libft/includes/libft.h"
+# include <limits.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/signal.h>
+# include <sys/vlimit.h>
+
+# include "shell_error.h"
+# include "ast.h"
+# include "token.h"
+# include "utilities.h"
+
+char **env;
+
+/*
+**	Lexer
+*/
+
+t_token	*lexical_analysis(char *input);
+
+/*
+**	Parser
+*/
+
+t_astnode	*syntax_analysis(t_token *token);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/sh.h"
+#include "minishell.h"
 
 void		get_prompt(void)
 {
@@ -21,18 +21,6 @@ void		get_prompt(void)
 	getcwd(pwd, PATH_MAX);
 	ft_putstr("Minishell ");
 	if ((home = get_env("HOME=", VAL)))
-		/*'get_env' function is simillar with your 'ft_find_env'
-		  but the diffrence is, it takes extra parameter 'VAL' or 'KEY'
-		  It gets address of very first character in env var when it comes with
-		  'KEY' macro.
-		  And if it's 'VAL', It returns first address of value
-		  For example, in this case, It returns /Users/ihwang
-		  But instead of mine, we can use builtin function 'getenv' from now on
-		  the result seems same with mine.
-		  Additionally, in minishell, every 'get_env' function is surrounded by
-		  'if' statement to protect the cases that there is no such env var.
-		  This is why I failed on evaluation.
-		*/
 	{
 		if (!ft_strcmp(pwd, home))
 			ft_putstr(pwd);
@@ -63,9 +51,6 @@ static char	**set_env(char **sample)
 	i = -1;
 	while (sample[++i])
 	{
-		// from du: i think you should need a bigger bufer for the env for the future, 
-		//because, this PATH_MAX macro just for the length of the path, we all so have
-		//the name of the variable plus '=' as well.
 		env[i] = (char*)malloc(sizeof(char) * PATH_MAX);
 		ft_strcat(env[i], sample[i]);
 	}
@@ -73,6 +58,54 @@ static char	**set_env(char **sample)
 	return (env);
 }
 
+/*
+** ================= NEW=====================================
+*/
+
+void executor(t_astnode *ast)  // fore debug
+{
+	ft_printf("We have AST\n");
+	(void)ast;
+}
+
+static void	ft_execute(char *input)
+{
+	t_token	*tokens;
+	t_astnode *ast;
+	char	*trimmed_input;
+
+	ast = NULL;
+	trimmed_input = ft_strtrim(input);
+	if (trimmed_input)
+	{
+		if ((tokens = lexical_analysis(trimmed_input)) != NULL)
+			if ((ast = syntax_analysis(tokens)) != NULL)
+				executor(ast);
+		free(trimmed_input); // need to free tokens after, i can free them all at executor
+	}
+}
+
+/*
+** ============================================================
+*/
+
+// OLD
+// static int	minishell(void)
+// {
+// 	char	*input;
+
+// 	while (1)
+// 	{
+// 		sig_controller(PARENT);
+// 		WIFSIGNALED(g_status) ? 0 : get_prompt();
+// 		g_status = 0;
+// 		input = get_input((int)1);
+// 		is_eof(input) ? parse_line(&input) : ft_exit(NULL, PRINT);
+// 	}
+// 	return (0);
+// }
+
+// NEW
 static int	minishell(void)
 {
 	char	*input;
@@ -80,19 +113,10 @@ static int	minishell(void)
 	while (1)
 	{
 		sig_controller(PARENT);
-		/*There are two macros PARENT and CHILD for this function
-		since all the signal controlings have to be changed to default setting
-		when it comes with child process*/
 		WIFSIGNALED(g_status) ? 0 : get_prompt();
 		g_status = 0;
 		input = get_input((int)1);
-		is_eof(input) ? parse_line(&input) : ft_exit(NULL, PRINT);
-		/*I'm using ft_exit function when user wants to exit, and even in error case.
-		  especially, ft_exit takes 'ER'macro when 'execve' is incomplete
-		  otherwise, it takes 'PRINT' macro*/
-		/*
-		   'is_eof' function is for checking if the given input is EOF or not
-		*/
+		is_eof(input) ? ft_execute(input) : ft_exit(NULL, PRINT);
 	}
 	return (0);
 }
